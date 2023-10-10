185c8b8e3dad0f09f0283324060a63c6958e0347
DrRichBruce <144108248+DrRichBruce@users.noreply.github.com>
2023-10-10 17:25:49 +0100

    Update weather-pi-demo.py

--- weather-pi-demo.py
+++ weather-pi-demo.py
@@ -83,3 +83,84 @@ GPIO.setmode(GPIO.BCM)
 # with a "PULL UP", which weakly pulls the input signal to 3.3V.
 GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# "handle_button" will be called every time a button is pressed
+# It receives one argument: the associated input pin.
+def handle_button(pin):
+    label = LABELS[BUTTONS.index(pin)]
+    print("Button press detected on pin: {} label: {}".format(pin, label))
+    if label == 'A':
+        message = "Data collection started! \nTurn off to stop. \nY: Energy saving mode"
+        
+        # Redraw the screen with the new message
+        size_x, size_y = draw.textsize(message, font)
+        x = (WIDTH - size_x) / 2
+        y = (HEIGHT / 2) - (size_y / 2)
+        draw.rectangle((0, 0, WIDTH, HEIGHT), back_colour)
+        draw.text((x, y), message, font=font, fill=text_colour)
+        disp.display(img)
+        
+        # Create a WeatherHat instance
+        sensor = weatherhat.WeatherHAT() 
+
+        # Create csv file for data collection
+        file_path = '/home/pi/weatherdata.csv'
+
+        # Check if the file already exists
+        if not os.path.exists(file_path):
+            # If not, create it and write the header
+            with open(file_path, 'w', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(['Date','Time','Compensated Temperature','Uncompensated Temperature','Relative Humidity', 'Humidity', 'Pressure','Light (Lux)'])
+        
+        # Data collection loop
+        try:
+            while True:
+                sensor.temperature_offset = OFFSET
+                sensor.update(interval=60.0) # This can be adjusted (in seconds) to meet your needs. Note: you must also adjust the time.sleep on line 51
+                timestamp = datetime.now()
+                
+                with open(file_path, mode="a", newline="") as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow([
+                    timestamp.strftime("%Y-%m-%d"),
+                        timestamp.strftime("%H:%M:%S"),
+                        sensor.temperature,
+                        sensor.device_temperature,
+                        sensor.relative_humidity,
+                        sensor.humidity,
+                        sensor.pressure,
+                        sensor.lux
+                    ])
+                print("Data collected")
+                time.sleep(60.0)
+                
+        except KeyboardInterrupt:
+            print("Data collection stopped.")
+
+    elif label == 'B':
+        # Code to execute when button B is pressed
+        
+        process = subprocess.Popen(["python3", "weather.py"])
+        # Wait for 10 seconds
+        time.sleep(10)
+        # Terminate the process after 30 seconds
+        process.terminate()
+
+        message = "A: Record data. Turn off to stop. \nB: Display data (30 secs). \nY: Energy saving mode."
+        size_x, size_y = draw.textsize(message, font)
+        
+    elif label == 'Y':
+        # Code to execute when button Y is pressed
+        # Turn off backlight on Press Y
+        disp.set_backlight(0)
+        
+    elif label == 'X':
+        # Code to execute when button X is pressed
+        # Turn off backlight on Press X
+        disp.set_backlight(12)
+        
+# Loop through out buttons and attach the "handle_button" function to each
+# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
+# picking a generous bouncetime of 100ms to smooth out button presses.
+for pin in BUTTONS:
+    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
