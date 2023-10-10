1bcd1047bb24dbb8949271fe914129c5b5d46cb0
DrRichBruce <144108248+DrRichBruce@users.noreply.github.com>
2023-10-10 17:38:49 +0100

    Update weather-pi-demo.py

--- weather-pi-demo.py
+++ weather-pi-demo.py
@@ -26,70 +26,66 @@ from weatherhat import history
 # -17.5oC is an estimated error of having the HAT attached directly to the RPi that gets hot when turned on
 OFFSET = 0 # Note: this is an estimate, you may need to adjust this value based on your needs
 
-while True:
-    # Display settings
-    FPS = 10
-    
-    SPI_SPEED_MHZ = 80
-    
-    # Create LCD class instance.
-    disp = ST7789.ST7789(
-    rotation=90,
-    port=0,
-    cs=1,
-    dc=9,
-    backlight=12,
-    spi_speed_hz=SPI_SPEED_MHZ * 1000 * 1000
-    )
-    
-    # Initialize display.
-    disp.begin()
-    
-    # Width and height to calculate text position.
-    WIDTH = disp.width
-    HEIGHT = disp.height
-    
-    # New canvas to draw on.
-    img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
-    draw = ImageDraw.Draw(img)
-    
-    # Text settings.
-    font_size = 15
-    font = ImageFont.truetype(UserFont, font_size)
-    text_colour = (255, 255, 255)
-    back_colour = (0, 170, 170)
-    
-    message = "A: Record data. \nTurn off to stop. \nB: Display data (30 secs). \nY: Energy saving mode."
-    size_x, size_y = draw.textsize(message, font)
-    
-    # Calculate text position
-    x = (WIDTH - size_x) / 2
-    y = (HEIGHT / 2) - (size_y / 2)
-    
-    # Draw background rectangle and write text.
-    draw.rectangle((0, 0, WIDTH, HEIGHT), back_colour)
-    draw.text((x, y), message, font=font, fill=text_colour)
-    disp.display(img)
-    
-    pass
-    time.sleep(1)
-    
-# The buttons on Weather HAT are connected to pins 5, 6, 16 and 24
-BUTTONS = [5, 6, 16, 24]
+# Display settings
+FPS = 10
 
-# These correspond to buttons A, B, X and Y respectively
-LABELS = ['A', 'B', 'X', 'Y']
+SPI_SPEED_MHZ = 80
 
-# Set up RPi.GPIO with the "BCM" numbering scheme
-GPIO.setmode(GPIO.BCM)
+# Create LCD class instance.
+disp = ST7789.ST7789(
+rotation=90,
+port=0,
+cs=1,
+dc=9,
+backlight=12,
+spi_speed_hz=SPI_SPEED_MHZ * 1000 * 1000
+)
 
-# Buttons connect to ground when pressed, so we should set them up
-# with a "PULL UP", which weakly pulls the input signal to 3.3V.
-GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Initialize display.
+disp.begin()
 
+# Width and height to calculate text position.
+WIDTH = disp.width
+HEIGHT = disp.height
+
+# New canvas to draw on.
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+
+# Text settings.
+font_size = 15
+font = ImageFont.truetype(UserFont, font_size)
+text_colour = (255, 255, 255)
+back_colour = (0, 170, 170)
+
+message = "A: Record data. \nTurn off to stop. \nB: Display data (30 secs). \nY: Energy saving mode."
+size_x, size_y = draw.textsize(message, font)
+
+# Calculate text position
+x = (WIDTH - size_x) / 2
+y = (HEIGHT / 2) - (size_y / 2)
+
+# Draw background rectangle and write text.
+draw.rectangle((0, 0, WIDTH, HEIGHT), back_colour)
+draw.text((x, y), message, font=font, fill=text_colour)
+disp.display(img)
+    
 # "handle_button" will be called every time a button is pressed
 # It receives one argument: the associated input pin.
 def handle_button(pin):
+    # The buttons on Weather HAT are connected to pins 5, 6, 16 and 24
+    BUTTONS = [5, 6, 16, 24]
+    
+    # These correspond to buttons A, B, X and Y respectively
+    LABELS = ['A', 'B', 'X', 'Y']
+    
+    # Set up RPi.GPIO with the "BCM" numbering scheme
+    GPIO.setmode(GPIO.BCM)
+    
+    # Buttons connect to ground when pressed, so we should set them up
+    # with a "PULL UP", which weakly pulls the input signal to 3.3V.
+    GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     label = LABELS[BUTTONS.index(pin)]
     print("Button press detected on pin: {} label: {}".format(pin, label))
     if label == 'A':
